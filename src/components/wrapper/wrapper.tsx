43359040545a50e27ad6bacf0e06435aa523128f
coreymaroney <27153173+coreymaroney@users.noreply.github.com>
2024-11-12 19:35:14 -0500

    Built out temporary page wrapper

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -1,8 +1,18 @@
 const Wrapper = () => {
   return (
-    <>
-      <></>
-    </>
+    <div className="flex flex-col px-4">
+      <div className="flex flex-row justify-between">
+        <div className="">logo</div>
+        <div className="">_ [] X</div>
+      </div>
+      <div className="flex flex-row gap-4 ">
+        <p>Home</p>
+        <p>View</p>
+        <p>Friends</p>
+        <p>Games</p>
+        <p>Help</p>
+      </div>
+    </div>
   );
 };
 
