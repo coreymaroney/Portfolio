09805396283527cf6067b40b42c7a5624734a416
coreymaroney <27153173+coreymaroney@users.noreply.github.com>
2024-11-12 19:34:26 -0500

    Page cleanout: - Removed font class and commented out boilerplate code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-tahoma)]">
-      <main className=""></main>
-      {/* <footer className="">
-        <a className="">
-          Test
-        </a>
-      </footer> */}
+    <div className="">
+      <main className="">test</main>
     </div>
   );
 }
