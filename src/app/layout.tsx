bfda1fb096b321f99cfe02366ef1b0ba894d60da
coreymaroney <27153173+coreymaroney@users.noreply.github.com>
2024-11-12 19:33:44 -0500

    Layout updates: - Introduce font in layout (in case I decide to add themes down the line) - Wrapped page content in div to create border

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Wrapper } from "../components";
-import localFont from "next/font/local";
 import "./globals.css";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Wrapper } from "../components";
 
 const tahoma = localFont({
   src: "./fonts/tahoma.ttf",
   variable: "--font-tahoma",
-  weight: "100 900",
+  // weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -21,9 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${tahoma.variable}`}>
+      <body
+        className={`${tahoma.variable} font-[family-name:var(--font-tahoma)]`}
+      >
         <Wrapper />
-        {children}
+        <div className="border-2 border-[--BorderBright] mx-2 px-4">
+          {children}
+        </div>
       </body>
     </html>
   );
